418501d51e5be8207e193349c2e32a72cc5789e1
Marta Apanasenka <ameowe@gmail.com>
2016-08-25 15:18:23 +0300

    Finish constructor & modify push

--- src/queue.js
+++ src/queue.js
@@ -9,11 +9,25 @@ class PriorityQueue {
   		} else {
    			this.maxSize = 30;
   		}
+  		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) 
 	{
-
+		this.heap.push(data, priority);
+		if (this.maxSize != null) 
+		{
+			if (this.size() >= this.maxSize) 
+			{
+				try 
+				{
+  					throw new Error('Out of queue capacity');
+				} catch (e) 
+				{
+  					alert(e.name + ': ' + e.message);
+				}
+			}
+		}
 	}
 
 	shift() 
