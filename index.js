220c5e35ef4e1641eaef88bd624b73b0e9e2d242
Marta Apanasenka <ameowe@gmail.com>
2016-08-30 19:15:33 +0300

    Modified swapWithParent

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const MaxHeap = require('./src/max-heap');
+const Node = require('./src/node');
 
-const h = new MaxHeap();
-window.h = h;
+const parentOfParent = new Node(100, 500);
+const parent = new Node(15, 42);
+const child = new Node(42, 15);
+
+parentOfParent.appendChild(parent);
+parent.appendChild(child);
+child.swapWithParent();
+
+let e1 = child.parent;
+let e2 = parentOfParent;
+//expect(child.parent).to.equal(parentOfParent);
--- src/node.js
+++ src/node.js
@@ -10,12 +10,12 @@ class Node {
 	appendChild(node) {
 			if (this.left == null) 
 				{
-					node.parent = this;
-					this.left = node;	
+					this.left = node;
+					node.parent = this;	
 				} else if (this.right == null) 
 				{ 
-					node.parent = this;
 					this.right = node;
+					node.parent = this;
 				}
 		}
 	
@@ -50,8 +50,19 @@ class Node {
 		if (this.parent != null) 
 		{
 			let pp = this.parent.parent;
+			let p = this.parent;
+			let r = this.right;
+			let l = this.left;
+			
 			this.parent.parent = this;
 			this.parent = pp;
+			if (p.right != null && p.right == this) 
+			{
+				p.left.parent = this;
+			} else if (p.left != null && p.left == this ) { 
+				p.right.parent = this; 
+			}
+
 		}
 	}
 }
